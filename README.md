a17c45ac5acf36d47480c6e0448cd3df7a3de17a
Dewdrops <v_v_4474@126.com>
2014-01-26 17:23:52 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
+English is not my first language, so feel free to correct me of any mistake.
+
 evil-extra-operator
 ===================
 
 Evil operator for evaluating codes, translating text, taking notes, searching via google, etc.
+
+
+Installation
+------------
+
+```lisp
+;; change default key bindings (if you want) HERE
+;; (setq evil-extra-operator-eval-key (kbd "ge"))
+(require 'evil-extra-operator)
+(global-evil-extra-operator-mode 1)
+```
+The key binding customization must be placed BEFORE `(require 'evil-extra-operator)`.
+
+
+Commands and default bindings
+-----------------------------
+
+`gr` (evil-operator-eval)
+
+Operator for evaluating code, i.e., use `grip` to evaluate current paragraph, `grr` to evaluate current line, or if you are in visual state, `gr` to evaluate current active region.
+You can customize option `evil-extra-operator-eval-modes-alist` to let evil-operator-eval support more language. Example:
+```lisp
+(setq evil-extra-operator-eval-modes-alist
+  '((ruby-mode ruby-send-region)
+    (enh-ruby-mode ruby-send-region)))
+```
+See document for this option for details.
+
+
+`gG` (evil-operator-google-search)
+
+Operator to search the web via google.
+
+
+`g.` (evil-operator-google-translate)
+
+Operator to translate text via google translate.
+To use this command, you should have [google-translate](https://github.com/manzyuk/google-translate) installed.
+
+
+`gh` (evil-operator-highlight)
+
+Operator to highlight region.
+To use this command, you should have [highlight.el](http://www.emacswiki.org/emacs-en/download/highlight.el) installed.
+You can use `hlt-eraser-mouse` or `hlt-eraser` which are provided by highlight.el to disable highlight
+
+
+`gs` (evil-operator-fold)
+
+Operator to fold region.
+To use this command, you should have [fold-this.el](https://github.com/magnars/fold-this.el) installed.
+You can use `fold-this-unfold-at-point` or `fold-this-unfold-all` which are provided by fold-this.el for unfolding.
+
+
+`go` (evil-operator-org-capture)
+
+Operator form of org-capture.
+So you can use `goip` to capture current paragraph.
+
+
+`gR` (evil-operator-remember)
+
+Like evil-operator-org-capture, but use remember-region instead of org-capture.
---
+++ evil-extra-operator.el
@@ -0,0 +1,214 @@
+;;; evil-extra-operator.el --- Evil operator for evaluating codes, taking notes, searching via google, etc.
+
+;; Copyright (C) 2014 by Dewdrops
+
+;; Author: Dewdrops <v_v_4474@126.com>
+;; URL: http://github.com/Dewdrops/evil-extra-operator
+;; Version: 0.1
+;; Keywords: evil, plugin
+;; Package-Requires: ((evil "1.0.7"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This package provides some extra operators for Emacs Evil, to evaluate codes,
+;; search via google, translate text, folding region, etc.
+;;
+;; Commands provided by this package:
+;; evil-operator-eval, evil-operator-google-translate,
+;; evil-operator-google-search, evil-operator-highlight, evil-operator-fold,
+;; evil-operator-org-capture, evil-operator-remember 
+;;
+;; Installation:
+;;
+;; put evil-extra-operator.el somewhere in your load-path and add these
+;; lines to your .emacs:
+;; (require 'evil-extra-operator)
+;; (global-evil-extra-operator-mode 1)
+
+;;; Code:
+
+(require 'evil)
+
+(defgroup evil-extra-operator nil
+  "Extra operator for evil"
+  :prefix "evil-extra-operator"
+  :group 'evil)
+
+(defcustom evil-extra-operator-google-translate-key (kbd "g.")
+  "Default binding for evil-operator-google-translate."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-google-search-key (kbd "gG")
+  "Default binding for evil-operator-google-search."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-eval-key (kbd "gr")
+  "Default binding for evil-operator-eval."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-highlight-key (kbd "gh")
+  "Default binding for evil-operator-highlight."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-fold-key (kbd "gs")
+  "Default binding for evil-operator-fold."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-org-capture-key (kbd "go")
+  "Default binding for evil-operator-org-capture."
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-remember-key (kbd "gR")
+  "Default binding for evil-operator-remember"
+  :type 'key-sequence
+  :group 'evil-extra-operator)
+
+(defcustom evil-extra-operator-eval-modes-alist
+  '((ruby-mode ruby-send-region)
+    (enh-ruby-mode ruby-send-region))
+  "Alist used to determine evil-operator-eval's behaviour.
+Each element of this alist should be of this form:
+
+ (MAJOR-MODE EVAL-FUNC [ARGS...])
+
+MAJOR-MODE denotes the major mode of buffer. EVAL-FUNC should be a function
+with at least 2 arguments: the region beginning and the region end. ARGS will
+be passed to EVAL-FUNC as its rest arguments"
+  :type '(alist :key-type symbol)
+  :group 'evil-extra-operator)
+
+;;;###autoload
+(evil-define-operator evil-operator-eval (beg end)
+  "Evil operator for evaluating code."
+  :move-point nil
+  (interactive "<r>")
+  (let* ((ele (assoc major-mode evil-extra-operator-eval-modes-alist))
+         (f-a (cdr-safe ele))
+         (func (car-safe f-a))
+         (args (cdr-safe f-a)))
+    (if (fboundp func)
+        (apply func beg end args)
+      (eval-region beg end))))
+
+;;;###autoload
+(evil-define-operator evil-operator-google-translate (beg end type)
+  "Evil operator for translating text via google translate."
+  :move-point nil
+  (interactive "<R>")
+  (require 'google-translate)
+  (let* ((langs (google-translate-read-args nil nil))
+         (source-language (car langs))
+         (target-language (cadr langs)))
+    (google-translate-translate
+     source-language target-language
+     (.eeo/make-url-args beg end type))))
+
+;;;###autoload
+(evil-define-operator evil-operator-google-search (beg end type)
+  "Evil operator for google search."
+  :move-point nil
+  (interactive "<R>")
+  (browse-url
+   (concat "http://www.google.com/search?q="
+           (url-hexify-string
+            (.eeo/make-url-args beg end type)))))
+
+(defun .eeo/make-url-args (beg end type)
+  (if (eq type 'block)
+      (let ((s nil))
+        (evil-apply-on-block
+         (lambda (b e) (setq s (cons (buffer-substring-no-properties b e) s)))
+         beg end nil)
+        (mapconcat 'identity (nreverse s) " "))
+    (setq s (buffer-substring-no-properties beg end))))
+
+;;;###autoload
+(evil-define-operator evil-operator-highlight (beg end type)
+  "Evil operator for region highlight."
+  :move-point nil
+  (interactive "<R>")
+  (require 'highlight)
+  (if (eq type 'block)
+      (evil-apply-on-block #'hlt-highlight-region beg end nil)
+    (hlt-highlight-region beg end)))
+
+;;;###autoload
+(evil-define-operator evil-operator-fold (beg end type)
+  "Evil operator for folding region."
+  :move-point nil
+  (interactive "<R>")
+  (require 'fold-this)
+  (if (eq type 'block)
+      (evil-apply-on-block #'fold-this beg end nil)
+    (fold-this beg end)))
+
+;;;###autoload
+(evil-define-operator evil-operator-org-capture (beg end)
+  "Evil operator for org-capture."
+  (interactive "<r>")
+  (require 'org)
+  (unless (region-active-p)
+    (goto-char beg)
+    (set-mark-command nil)
+    (goto-char end))
+  (org-capture))
+
+;;;###autoload
+(evil-define-operator evil-operator-remember (beg end)
+  "Evil operator for remember-region"
+  :move-point nil
+  (interactive "<r>")
+  (remember-region beg end))
+
+(defvar evil-extra-operator-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map evil-extra-operator-google-translate-key 'evil-operator-google-translate)
+    (define-key map evil-extra-operator-google-search-key 'evil-operator-google-search)
+    (define-key map evil-extra-operator-eval-key 'evil-operator-eval)
+    (define-key map evil-extra-operator-fold-key 'evil-operator-fold)
+    (define-key map evil-extra-operator-highlight-key 'evil-operator-highlight)
+    (define-key map evil-extra-operator-org-capture-key 'evil-operator-org-capture)
+    (define-key map evil-extra-operator-remember-key 'evil-operator-remember)
+    map))
+
+;;;###autoload
+(define-minor-mode evil-extra-operator-mode
+  "Buffer local minor mode to enable extra operators for Evil."
+  :lighter ""
+  :keymap evil-extra-operator-mode-map
+  (evil-normalize-keymaps))
+
+(defun evil-extra-operator-mode-install () (evil-extra-operator-mode 1))
+
+;;;###autoload
+(define-globalized-minor-mode global-evil-extra-operator-mode
+  evil-extra-operator-mode evil-extra-operator-mode-install
+  "Global minor mode of extra operator for Evil.")
+
+(evil-make-overriding-map evil-extra-operator-mode-map)
+
+(provide 'evil-extra-operator)
+;;; evil-extra-operator.el ends here
