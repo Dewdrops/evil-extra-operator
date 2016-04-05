2c87b8a89061af46db313ed7590c2f5d6f9a2b97
Tal Wrii <talwrii@googlemail.com>
2016-04-05 14:46:34 -0600

    clone -- do what I mean whitespace beheviour
    Ensure that we add a single-newline at the end of a
    string if we inserting multiple lines  for e.g
    
    ```
    (defun func (val)
    
       val)
    ```

--- evil-extra-operator.el
+++ evil-extra-operator.el
@@ -236,12 +236,22 @@ be passed to EVAL-FUNC as its rest arguments"
 (evil-define-operator evil-operator-clone (beg end type)
   "Evil operator to create a clone of a motion"
   :move-point nil
-  (interactive "<r>")
-  (let (
-        (content (filter-buffer-substring beg end nil)))
+  (interactive "<R>")
+  (let* (
+        (content (filter-buffer-substring beg end nil))
+        (contains-new-line (not (null (string-match "\n" content))))
+        (ends-with-newline (not (null (string-match "\n\\'" content))))
+        (new-line-spec (list contains-new-line ends-with-newline)))
+
+    (message (format "%S" type))
+    (message (format "%S" content))
     (goto-char beg)
-    (beginning-of-line)
-    (insert (concat content "\n"))))
+    (insert
+     (cond
+      ((equal new-line-spec '(t t)) content)
+      ((equal new-line-spec '(t nil)) (concat content "\n"))
+      ((equal new-line-spec '(nil nil)) content)
+      (t content)))))
 
 
 ;;;###autoload
